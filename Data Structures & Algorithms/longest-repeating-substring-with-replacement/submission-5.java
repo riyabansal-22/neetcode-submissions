c18ef828ff8cc3be6ca5c5bcdd50999794bcae84
riyabansal-22 <165152853+riyabansal-22@users.noreply.github.com>
2026-06-24 16:46:09 +0530

    Add: longest-repeating-substring-with-replacement - submission-5

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-5.java
@@ -0,0 +1,107 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        //optimal
+        int l=0, maxF =0;
+        Map<Character, Integer> freq = new HashMap<>();
+        int maxLength = 0;
+        for(int r=0; r<s.length(); r++){
+            freq.put(s.charAt(r), freq.getOrDefault(s.charAt(r), 0)+1);
+            maxF = Math.max(maxF, freq.get(s.charAt(r)));
+
+            if((r-l+1) - maxF > k){
+                freq.put(s.charAt(l), freq.get(s.charAt(l))-1);
+                // if(freq.get(s.charAt(l))== 0){
+                //     freq.remove(s.charAt(l));
+                // }
+                l++;
+            }
+            if((r-l+1)-maxF <= k){
+                maxLength = Math.max(maxLength, r-l+1);
+            }
+        }
+        return maxLength;
+
+        //better T.C-> O(n)+O(n)*26 =O(2n), S.C->o(26) 
+        // int l=0, maxF=0;
+        // int[] hash = new int[26];
+        // int maxLength = 0;
+        // for(int r=0; r<s.length(); r++){
+        //     hash[s.charAt(r)-'A']++;
+        //     maxF = Math.max(maxF, hash[s.charAt(r)-'A']);
+
+        //     while((r-l+1) - maxF > k){
+        //         hash[s.charAt(l)-'A']--;
+        //         maxF = 0;
+        //         for(int i=0; i<26; i++){
+        //             maxF = Math.max(maxF, hash[i]);
+        //         }
+        //         l++;
+        //     }
+        //     if((r-l+1)-maxF <=k){
+        //         maxLength = Math.max(maxLength, r-l+1);
+        //     }
+        // }
+        // return maxLength;
+
+
+        //better (using hashMap) T.C->O(2n), S.C->O(n)
+        // int l=0, maxF = 0;
+        // int maxLength = 0;
+        // Map<Character, Integer> freq = new HashMap<>();
+        // for(int r=0; r<s.length(); r++){
+        //     char ch = s.charAt(r);
+        //     freq.put(ch, freq.getOrDefault(ch, 0)+1);
+        //     maxF = Math.max(maxF, freq.getOrDefault(ch, 0));
+        //         while((r-l+1) - maxF > k){
+        //             freq.put(s.charAt(l), freq.get(s.charAt(l)) -1);
+        //             if(freq.get(s.charAt(l)) == 0){
+        //                 freq.remove(s.charAt(l));
+        //             }
+        //             maxF = 0;
+        //             maxF = Math.max(maxF, freq.get(s.charAt(r)));
+        //             l++;
+        //         }
+            
+        //     maxLength = Math.max(maxLength, r-l+1);
+        // }
+        // return maxLength;
+
+        //BRUTE T.C-->O(n2), S.C-->O(26)
+        // int maxLength = 0;
+        // for(int i=0; i<s.length(); i++){
+        //     int[] hash = new int[26];
+        //     int maxF = 0;
+        //     for(int j=i; j<s.length(); j++){
+        //         hash[s.charAt(j)-'A']++;
+        //         maxF = Math.max(maxF, hash[s.charAt(j)-'A']);
+        //         int changes = (j-i+1) - maxF;
+        //         if(changes<=k){
+        //             maxLength = Math.max(maxLength, j-i+1);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return maxLength;
+
+        // int maxLength = 0;
+        // for(int i=0; i<s.length(); i++){
+        //     Map<Character, Integer> freq = new HashMap<>();
+        //     int maxF = 0;
+        //     for(int j=i; j<s.length(); j++){
+        //         char ch = s.charAt(j);
+        //         freq.put(ch, freq.getOrDefault(ch, 0)+1);
+        //         maxF = Math.max(maxF, freq.getOrDefault(ch, 0));
+        //         int changes = (j-i+1) - maxF;
+        //         if(changes<=k){
+        //             maxLength = Math.max(maxLength, j-i+1);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return maxLength;
+        
+       
+    }
+}
