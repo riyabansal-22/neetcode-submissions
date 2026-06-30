171d4c97e14cf28607f226da3916761c8ae47b10
riyabansal-22 <165152853+riyabansal-22@users.noreply.github.com>
2026-06-30 11:11:42 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,98 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+        //OPTIMAL
+        int l=0;
+        int minLength = Integer.MAX_VALUE;
+        int sIndex = -1, count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        int[] hash =new int[256];
+        for(int i=0; i<t.length(); i++){
+            hash[t.charAt(i)]++;
+        }
+        for(int r=0; r<s.length(); r++){
+            if(hash[s.charAt(r)] > 0){
+                count++;
+            }
+            hash[s.charAt(r)]--;
+            while(count==t.length()){
+                if(r-l+1 < minLength){
+                    minLength = r-l+1;
+                    sIndex = l;
+                }
+                hash[s.charAt(l)]++;
+                if(hash[s.charAt(l)] > 0){
+                    count--;
+                }
+                l++;
+            }
+        }
+        return sIndex == -1 ? "" : s.substring(sIndex, sIndex+minLength);
+
+
+        //brute ---> hashMap
+        // if(t.isEmpty()){
+        //     return "";
+        // }
+        // Map<Character, Integer> countT = new HashMap<>();
+        // for(char c: t.toCharArray()){
+        //     countT.put(c, countT.getOrDefault(c, 0)+1);
+        // }
+
+        // int[] res = {-1, -1};
+        // int resLen = Integer.MAX_VALUE;
+
+        // for(int i=0; i<s.length(); i++){
+        //     Map<Character, Integer> countS = new HashMap<>();
+        //     for(int j=i; j<s.length(); j++){
+        //         countS.put(s.charAt(j), countS.getOrDefault(s.charAt(j), 0)+1);
+
+        //         boolean flag = true;
+        //         for(char c: countT.keySet()){
+        //             if(countS.getOrDefault(c, 0) < countT.get(c)){
+        //                 flag = false;
+        //                 break;
+        //             }
+        //         }
+        //         if(flag && j-i+1<resLen){
+        //             resLen = j-i+1;
+        //             res[0] = i;
+        //             res[1] = j;
+        //         }
+        //     }
+        // }
+        // return resLen == Integer.MAX_VALUE ? "" : s.substring(res[0], res[1]+1);
+
+
+        //brute T.C-->O(N2*256)  hash array
+        // int minLength = Integer.MAX_VALUE;
+        // int startIndex = -1;
+
+        // for(int i=0; i<s.length(); i++){
+        //     int[] hash = new int[256];
+        //     int count = 0;
+
+        //     for(int j=0; j<t.length(); j++){
+        //         hash[t.charAt(j)]++;
+        //     }
+
+        //     for(int j=i; j<s.length(); j++){
+        //         if(hash[s.charAt(j)]>0){
+        //             count++;
+        //         }
+        //         hash[s.charAt(j)]--;
+        //         if(count==t.length()){
+        //             if(j-i+1<minLength){
+        //                 minLength = j-i+1;
+        //                 startIndex = i;
+        //             }
+        //             break;
+        //         }
+        //     }
+        // }
+        // if(startIndex == -1){
+        //     return "";
+        // }
+        // return s.substring(startIndex, startIndex+minLength);
+    }
+}
